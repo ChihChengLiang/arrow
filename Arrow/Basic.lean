ca216352b7b6ad0bd08956a1ec886d3a2a91963a
ChihChengLiang <chihchengliang@gmail.com>
2026-02-22 13:31:49 +0800

    unanimity and AIIA

--- Arrow/Basic.lean
+++ Arrow/Basic.lean
@@ -1,1 +1,38 @@
+import Mathlib.Order.Defs.PartialOrder
+import Mathlib.Data.Finset.Defs
+
+variable (α: Type) -- α is the type of alternatives
+variable (N: ℕ ) -- N is the number of voters
+
+structure Preorder' (α : Type) where
+  le : α → α → Prop
+  refl : ∀ a, le a a
+  trans : ∀ a b c, le a b → le b c → le a c
+  total : ∀ a b, le a b ∨ le b a
+
+def Preorder'.lt {α : Type} (p : Preorder' α) (a b : α) : Prop :=
+  p.le a b ∧ ¬p.le b a
+
+-- Map individual i to their preferences
+def PreferenceProfile (α : Type) (N : ℕ) :=
+  Fin N → Preorder' α
+
+def SocialWelfareFunction (α : Type) (N : ℕ) :=
+  (Fin N → Preorder' α) → Preorder' α
+
+-- A constitution respects unanimity if society puts alternative `a` strictly above
+-- `b` whenever every individual puts `a` strictly above `b`.
+def unanimity (R : SocialWelfareFunction α N) : Prop :=
+  ∀ (profile: PreferenceProfile α N) (a b: α),
+    (∀ i: Fin N, (profile i).lt a b) -> (R profile).lt a b
+
+
+-- (AIIA: Arrow's Independence of Irrelevant Alternatives)
+-- If each individual's preferences over `a` and `b` are the same in profile `p` and profile `q`,
+-- then SocialWelfareFunction(p) and SocialWelfareFunction(q) rank the two alternatives the same
+def AIIA (R : SocialWelfareFunction α N) : Prop :=
+  ∀ (p q: PreferenceProfile α N) (a b: α),
+      (∀ i: Fin N, (p i).lt a b) ↔ (∀ i: Fin N, (q i).lt a b) ->
+      ((R p).lt a b ↔ (R q).lt a b)
+
 def hello := "world"
